6f85224dbb64fe9a6e4af47665caeb25e23ba3e0
Rishi <rishigangwar2011@gmail.com>
2026-06-15 21:27:17 +0530

    add inputvalidation, unit lables, and tensile/compressive detection

--- stress-caculator.py
+++ stress-caculator.py
@@ -1,6 +1,24 @@
-F=input('Enter Force: ')
 
-A=input('Enter area: ')
+while True:
+  try:
+    F=float(input("Enter force (N): "))
+    break
+  except ValueError:
+    print("Enter an numerical value")
+while True:
+  try:
+    A=float(input("Enter area (mm²): "))
+    if (A<=0):
+      print("Enter an positive value")
+      continue
+    break
+  except ValueError:
+    print("Enter an numerical value")
 
-sigma=float(F)/float(A)
-print(f"Axial Stress: {sigma:.2f} MPa")
+sigma=F/A
+if (F<0):
+	print(f"Axial stress: {sigma:.2f} MPa (Compressive)")
+elif (F>0):
+	print(f"Axial stress: {sigma:.2f} MPa (Tensile)")
+else:
+	print("No force Applied")
