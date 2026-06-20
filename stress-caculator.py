7d2ec567ed004c2aef3fe39c77c04c36c0b4d4d5
Rishi <rishigangwar2011@gmail.com>
2026-06-20 18:48:11 +0530

    Updated Stress calculator with functions for reusablity of code and function calling in stress-calculator.py

--- stress-caculator.py
+++ stress-caculator.py
@@ -1,24 +1,36 @@
+def Force():
+  while True:
+    try:
+      F=float(input("Enter force (N): "))
+      break
+    except ValueError:
+      print("Enter an numerical value")
+  return F
+def Area():
+  while True:
+    try:
+      A=float(input("Enter area (mm²): "))
+      if (A<=0):
+        print("Enter an positive value")
+        continue
+      break
+    except ValueError:
+      print("Enter an numerical value")
+  return A
 
-while True:
-  try:
-    F=float(input("Enter force (N): "))
-    break
-  except ValueError:
-    print("Enter an numerical value")
-while True:
-  try:
-    A=float(input("Enter area (mm²): "))
-    if (A<=0):
-      print("Enter an positive value")
-      continue
-    break
-  except ValueError:
-    print("Enter an numerical value")
+def stress(F,A):
+  sigma=F/A
+  if (F<0):
+    print(f"Axial stress: {sigma:.2f} MPa (Compressive)")
+  elif(F>0):
+    print(f"Axial stress: {sigma:.2f} MPa (Tensile)")
+  else:
+    print("No force Applied")
+  return sigma
 
-sigma=F/A
-if (F<0):
-	print(f"Axial stress: {sigma:.2f} MPa (Compressive)")
-elif (F>0):
-	print(f"Axial stress: {sigma:.2f} MPa (Tensile)")
-else:
-	print("No force Applied")
+F=Force()
+A=Area()
+stress(F,A)
+
+
+
