4d2aeaa26b5f61b5f0b7261ff51c5fc40d3db20b
Rishi <rishigangwar2011@gmail.com>
2026-06-21 19:59:31 +0530

    added connection for the postgress server and added feature to store the stress calculation data to the postgress databse and added columns for bending stresses in stress-caculator.py

--- stress-caculator.py
+++ stress-caculator.py
@@ -1,3 +1,17 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD")
+)
+cursor = conn.cursor()
 def Force():
   while True:
     try:
@@ -48,8 +62,10 @@ def stress(F,A):
   sigma=F/A
   if (F<0):
     print(f"Axial stress: {sigma:.2f} MPa (Compressive)")
+    cursor.execute("INSERT INTO stress_results (force_n, area_mm2, stress_mpa, stress_type) VALUES (%s,%s,%s,%s)", (F, A, sigma, 'Compressive'))
   elif(F>0):
     print(f"Axial stress: {sigma:.2f} MPa (Tensile)")
+    cursor.execute("INSERT INTO stress_results (force_n, area_mm2, stress_mpa, stress_type) VALUES (%s,%s,%s,%s)", (F, A, sigma, 'Tensile'))
   else:
     print("No force Applied")
   return sigma
@@ -57,19 +73,21 @@ def Bending_Stress(M, y, I):
   BS=M*y/I
   if (BS<0):
     print(f"Bending stress: {BS:.2f} MPa (Compressive)")
+    cursor.execute("INSERT INTO stress_results (bending_moment_nmm, neutral_axis_mm, moi_mm4, bending_stress_mpa, stress_type) VALUES (%s,%s,%s,%s,%s)", (M,y,I,BS, 'Compressive'))
   elif(BS>0):
     print(f"Bending stress: {BS:.2f} MPa (Tensile)")
+    cursor.execute("INSERT INTO stress_results (bending_moment_nmm, neutral_axis_mm, moi_mm4, bending_stress_mpa, stress_type) VALUES (%s,%s,%s,%s,%s)", (M,y,I,BS, 'Tensile'))
   else:
     print("No Moment Applied")
   return BS
 F=Force()
 A=Area()
-stress(F,A)
+sigma=stress(F,A)
 
 M=Moment()
 y=Neutral_Axis()
 I=MOI()
-Bending_Stress(M,y,I)
+BS=Bending_Stress(M,y,I)
 
-
-
+conn.commit()
+conn.close()
